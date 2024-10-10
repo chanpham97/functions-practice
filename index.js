7160f45bd193c8951ae9776ff58476ebc005a2d4
Chan Pham <iamchanpham@gmail.com>
2024-10-10 06:32:09 -0400

    Initial template

---
+++ index.js
@@ -0,0 +1,51 @@
+// Task 1: Welcomer
+// Define a function called greeting that takes a name as a parameter and console logs "Hello " + name
+
+
+// Call the greeting function with your name as the argument
+
+// Prompt the user to enter their name
+
+// Call the greeting function with the user's name as an argument
+
+
+// -------------------------------------------------
+// Task 2: Adder
+// Define a function called add that takes two numbers as parameters and returns the sum
+
+
+// Call the add function and pass in any two numbers to test and console log the result
+
+// Prompt the user to enter one number (make sure to cast it)
+
+// Prompt the user to enter another number (make sure to cast it)
+
+// Call the add function and pass in the user's numbers and console log the result
+
+
+// -------------------------------------------------
+// Task 3: Voter Eligibility
+// Define a function that takes in a user's age and tells them if they are old enough to vote
+
+
+
+// Call the voter eligibility function with a test number and console log the result
+
+// Prompt the user to enter their age (make sure to cast it) 
+
+// Call the voter eligibility function with the user's age and console log the result
+
+
+// -------------------------------------------------
+// Task 4: Message Repeater
+// Define a function that takes in a message and a number and repeats the message number times
+// e.g. message: "Open Sesame", number: 20 should print "Open Sesame" 20 times
+
+
+
+// Call the message repeater functino
+
+// Get user inputs and call it with user inputs
+
+
+
